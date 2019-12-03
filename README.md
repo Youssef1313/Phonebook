265f592f18408e41cb739e6ab3c83c8b4b594eff
Youssef Victor <youssefvictor00@gmail.com>
2019-12-03 20:08:57 +0200

    Added PhonebookEntry and Date structs with constructors

---
+++ README.md
@@ -0,0 +1,11 @@
+# Telephone Book Project in C
+
+My final project for programming 1 course
+
+## Coding style
+
+- Use PascalCase for functions (except for `main`), structs, and unions names.
+- Use camelCase for function parameters, local variables, structs members, and unions members.
+- Use ALL_UPPER_CASE_WITH_UNDERSCORE to define macros.
+- Use allman braces. Curly braces should be put in their own line.
+- Prefix pointer variables with `p`. For example, `Date *pBirthDate`, not `Date *birthDate`.
--- TelephoneBook/PhonebookEntry.h
+++ TelephoneBook/PhonebookEntry.h
@@ -1,6 +1,49 @@
 #pragma once
 
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_NAME_LENGTH 20
+#define MAX_ADDRESS_LENGTH 300
+#define MAX_EMAIL_LENGTH 50
+#define MAX_PHONE_LENGTH 15
+
 typedef struct
 {
+	short day;
+	short month;
+	short year;
+} Date;
 
+typedef struct
+{
+	char lastName[MAX_NAME_LENGTH];
+	char firstName[MAX_NAME_LENGTH];
+	Date birthDate;
+	char address[MAX_ADDRESS_LENGTH];
+	char email[MAX_EMAIL_LENGTH];
+	char phone[MAX_PHONE_LENGTH];
 } PhonebookEntry;
+
+PhonebookEntry *ConstructPhonebookEntry(char *lastName, char *firstName, Date birthDate, char *address, char *email, char *phone)
+{
+	PhonebookEntry *pEntry = malloc(sizeof(PhonebookEntry));
+	// TODO: check if allocation failed.
+	strcpy(pEntry->lastName, lastName); // TODO: Optimization. Prevent buffer overflow.
+	strcpy(pEntry->firstName, firstName); // TODO: Optimization. Prevent buffer overflow.
+	pEntry->birthDate = birthDate;
+	strcpy(pEntry->address, address); // TODO: Optimization. Prevent buffer overflow.
+	strcpy(pEntry->email, email); // TODO: Optimization. Prevent buffer overflow.
+	strcpy(pEntry->phone, phone); // TODO: Optimization. Prevent buffer overflow.
+	return pEntry;
+}
+
+Date *ConstructDate(short day, short month, short year)
+{
+	Date *pDate = malloc(sizeof(Date));
+	// TODO: check if allocation failed.
+	pDate->day = day;
+	pDate->month = month;
+	pDate->year = year;
+	return pDate;
+}
