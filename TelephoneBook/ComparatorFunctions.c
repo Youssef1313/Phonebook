d1dd5d76e6335b14489f35b7411dad48cd00ec58
Youssef Victor <youssefvictor00@gmail.com>
2019-12-06 16:27:44 +0200

    Add ComparatorFunctions header and source files

---
+++ TelephoneBook/ComparatorFunctions.c
@@ -0,0 +1,22 @@
+#include "PhonebookEntry.h"
+#include "ComparatorFunctions.h"
+
+int CompareEntriesByDate(const void *p1, const void *p2)
+{
+    PhonebookEntry **first = (PhonebookEntry **)p1;
+    PhonebookEntry **second = (PhonebookEntry **)p2;
+    return CompareDates((*first)->birthDate, (*first)->birthDate);
+}
+
+int CompareDates(Date date1, Date date2)
+{
+    if (date1.year > date2.year) return 1;
+    if (date1.year < date2.year) return -1;
+
+    if (date1.month > date2.month) return 1;
+    if (date1.month < date2.month) return -1;
+
+    if (date1.day > date2.day) return 1;
+    if (date1.day < date2.day) return -1;
+    return 0;
+}
---
+++ TelephoneBook/ComparatorFunctions.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "PhonebookEntry.h"
+
+int CompareEntriesByDate(const void *p1, const void *p2);
+int CompareDates(Date date1, Date date2);
